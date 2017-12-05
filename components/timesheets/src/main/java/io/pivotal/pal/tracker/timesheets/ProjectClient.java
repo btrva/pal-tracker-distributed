d2cdb160e39d05bfdc2e1d2adbd6c1374610bb08
Sergei Hanka <sergeiahanka@gmail.com>
2017-12-05 09:19:41 -0500

    added fallback methods

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,12 +1,16 @@
 package io.pivotal.pal.tracker.timesheets;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final RestOperations restOperations;
     private final String endpoint;
@@ -24,5 +28,10 @@ public class ProjectClient {
         projectCache.put(projectId, project);
 
         return project;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId){
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectCache.get(projectId);
     }
 }
